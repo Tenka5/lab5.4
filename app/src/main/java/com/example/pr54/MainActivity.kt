482553bef0e4519d55a40618b77a13d1f344882e
Vadim Teremetskiy <97560942+Tenka5@users.noreply.github.com>
2022-05-24 02:37:45 +0300

    first commit

---
+++ app/src/main/java/com/example/pr54/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.pr54
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        val text = findViewById<EditText>(R.id.editText)
+        button.setOnClickListener {
+            send(text.text.toString())
+        }
+    }
+
+
+    fun send(text:String){
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, text)
+            type = "text/plain"
+        }
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+}
